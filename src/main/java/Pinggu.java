b29ed9eecfbd687a0983daea5555bd4f47450496
Sinnez1 <99955247+Sinnez1@users.noreply.github.com>
2026-01-16 19:44:26 +0800

    Added add and list

--- src/main/java/Pinggu.java
+++ src/main/java/Pinggu.java
@@ -1,38 +1,56 @@
 import java.util.*;
 
 public class Pinggu {
+    private static List<String> tasks = new ArrayList<>();
+    private static final String DIVIDER = "____________________________________________________________";
     public static void main(String[] args) {
-
-        String text = "____________________________________________________________ \n"
+        Scanner scanner = new Scanner(System.in);
+        String text = DIVIDER + "\n"
                 + "Hello! I'm Pinggu \n"
                 + "What can I do for you? \n"
-                + "____________________________________________________________";
+                + DIVIDER;
         System.out.println(text);
 
-        Scanner scanner = new Scanner(System.in);
         while (true) {
             String input = scanner.nextLine();
 
             if (input.equals("bye")) {
                 printExit();
                 break;
+            } else if (input.equals("list")) {
+                listTasks();
+            } else {
+                printText(input);
+                addText(input);
             }
-            printText(input);
         }
     }
 
     private static void printText(String text) {
-        String output = "____________________________________________________________ \n"
-                + text
-                + " \n"
-                + "____________________________________________________________";
+        String output = DIVIDER + "\n"
+                + "added: " + text + " \n"
+                + DIVIDER;
         System.out.println(output);
     }
 
     private static void printExit() {
-        String output = "____________________________________________________________ \n"
+        String output = DIVIDER + "\n"
                 + "Bye. Hope to see you again soon! \n"
-                + "____________________________________________________________";
+                + DIVIDER;
         System.out.println(output);
+    }
+
+    private static void addText(String text) {
+        tasks.add(text);
+    }
+
+    private static void listTasks() {
+        System.out.println(DIVIDER);
+        int counter = 1;
+        for (String task : tasks) {
+            System.out.println(counter + " ." + task);
+            counter++;
+        }
+        System.out.println(DIVIDER);
     }
 }
