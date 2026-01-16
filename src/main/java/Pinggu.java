189b0b5322c30c3258c8b0bef52a948d66bbd5b0
Sinnez1 <99955247+Sinnez1@users.noreply.github.com>
2026-01-16 19:14:39 +0800

    Rename main class and add greeting

---
+++ src/main/java/Pinggu.java
@@ -0,0 +1,14 @@
+public class Pinggu {
+    public static void main(String[] args) {
+
+        String text = "____________________________________________________________ \n"
+                + "Hello! I'm Pinggu \n"
+                + "What can I do for you? \n"
+                + "____________________________________________________________ \n"
+                + "Bye. Hope to see you again soon! \n"
+                + "____________________________________________________________";
+
+
+        System.out.println(text);
+    }
+}
