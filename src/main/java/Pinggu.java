24e7c5eab5094ebf442909d8b22273859ae18665
Sinnez1 <99955247+Sinnez1@users.noreply.github.com>
2026-01-16 20:08:15 +0800

    Added Task class and mark as done feature

--- src/main/java/Pinggu.java
+++ src/main/java/Pinggu.java
@@ -1,8 +1,8 @@
 import java.util.*;
 
 public class Pinggu {
-    private static List<String> tasks = new ArrayList<>();
-    private static final String DIVIDER = "____________________________________________________________";
+    private static List<Task> tasks = new ArrayList<>();
+    public static final String DIVIDER = "____________________________________________________________";
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String text = DIVIDER + "\n"
@@ -13,15 +13,24 @@ public class Pinggu {
 
         while (true) {
             String input = scanner.nextLine();
-
             if (input.equals("bye")) {
                 printExit();
                 break;
             } else if (input.equals("list")) {
                 listTasks();
+            } else if (input.startsWith("mark")) {
+                String[] array = input.split(" ");
+                int taskNo = Integer.parseInt(array[1]) - 1; //0-indexing for List<>
+                Task task = tasks.get(taskNo);
+                task.markTask();
+            } else if (input.startsWith("unmark")) {
+                String[] array = input.split(" ");
+                int taskNo = Integer.parseInt(array[1]) - 1;
+                Task task = tasks.get(taskNo);
+                task.unmarkTask();
             } else {
                 printText(input);
-                addText(input);
+                addTask(input);
             }
         }
     }
@@ -40,15 +49,17 @@ public class Pinggu {
         System.out.println(output);
     }
 
-    private static void addText(String text) {
-        tasks.add(text);
+    private static void addTask(String input) {
+        Task task = new Task(input);
+        tasks.add(task);
     }
 
     private static void listTasks() {
         System.out.println(DIVIDER);
         int counter = 1;
-        for (String task : tasks) {
-            System.out.println(counter + " ." + task);
+        for (Task task : tasks) {
+            System.out.println(counter + "." + "[" + task.getStatusIcon() + "] "
+                    + task.description);
             counter++;
         }
         System.out.println(DIVIDER);
