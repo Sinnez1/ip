0647dc35e337e01d3c33075be233b5e0b7e4292f
Sinnez1 <99955247+Sinnez1@users.noreply.github.com>
2026-01-16 19:32:08 +0800

    Echo

--- src/main/java/Pinggu.java
+++ src/main/java/Pinggu.java
@@ -1,14 +1,38 @@
+import java.util.*;
+
 public class Pinggu {
     public static void main(String[] args) {
 
         String text = "____________________________________________________________ \n"
                 + "Hello! I'm Pinggu \n"
                 + "What can I do for you? \n"
-                + "____________________________________________________________ \n"
+                + "____________________________________________________________";
+        System.out.println(text);
+
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                printExit();
+                break;
+            }
+            printText(input);
+        }
+    }
+
+    private static void printText(String text) {
+        String output = "____________________________________________________________ \n"
+                + text
+                + " \n"
+                + "____________________________________________________________";
+        System.out.println(output);
+    }
+
+    private static void printExit() {
+        String output = "____________________________________________________________ \n"
                 + "Bye. Hope to see you again soon! \n"
                 + "____________________________________________________________";
-
-
-        System.out.println(text);
+        System.out.println(output);
     }
 }
