66cf1c8952371dd71191eba154cb79a10b397001
Sinnez1 <99955247+Sinnez1@users.noreply.github.com>
2026-02-01 18:52:17 +0800

    Add GUI class

---
+++ src/main/java/pinggu/ui/PingguGuiLauncher.java
@@ -0,0 +1,9 @@
+package pinggu.ui;
+
+import javafx.application.Application;
+
+public class PingguGuiLauncher {
+    public static void main(String[] args) {
+        Application.launch(PingguGui.class, args);
+    }
+}
