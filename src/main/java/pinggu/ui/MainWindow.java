6fa8208621e085a6132692bba8604bd2dd65534c
Sinnez1 <99955247+Sinnez1@users.noreply.github.com>
2026-02-02 16:50:04 +0800

    Add background

--- src/main/java/pinggu/ui/MainWindow.java
+++ src/main/java/pinggu/ui/MainWindow.java
@@ -38,6 +38,7 @@ public class MainWindow extends AnchorPane {
     @FXML
     public void initialize() {
         scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+        userInput.setPromptText("Type here...");
     }
 
     /**
